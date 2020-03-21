a71cc268e96fb530af82a3fbb58a7b00df3c25df
Christopher Osborn <z68ozz@gmail.com>
2020-03-21 00:47:37 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,21 @@
 require_relative "./vehicle.rb"
+require 'pry'
+class Car 
+  
+  attr_accessor :wheel_size, :wheel_number
+  attr_reader :number
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
+  def go 
+    return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+  
+  def fill_up_tank
+    return "filling up!"
+  end
+  
+end
+  
